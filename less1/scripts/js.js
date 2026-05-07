bf6a9e7c8d6be730955e7beeaaae1399b947dee0
kryvovan <kryvobokivan0707@gmail.com>
2026-05-07 11:45:20 +0300

    Add files via upload

---
+++ less1/scripts/js.js
@@ -0,0 +1,73 @@
+// console.log('Happy developing ✨')
+
+// document.write("Yuki, Ryo and Ryuko!");
+
+// console.log("Yuki, Ryo and Ryuko!");
+// console.info("Yuki, Ryo and Ryuko! 2");
+// console.error("No Yuki, Ryo and Ryuko!");
+// console.warn("Maybe Yuki, Ryo and Ryuko?");
+//
+// var MyFirstVar;
+// MyFirstVar = 10;
+// console.log(MyFirstVar);
+//
+// var MySecondVar = '23';
+// console.log(MySecondVar + MyFirstVar);
+// console.log(MySecondVar - MyFirstVar);
+//
+//
+// var int = 10;
+// var float = 15.5;
+// var bool = true; //false
+// var str = 'SkibidiRizzler Ryo'
+// var a;
+// a = null;
+// console.log(a);
+
+// let b = 10, a = b + 2;
+// a = 10;
+// var a;
+// console.log(a);
+
+
+// let a = 10;
+// let a = 20;
+
+// console.log(a);
+
+
+
+// const pi = 3.14;
+//
+//
+// let a =10, b = 3;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+// //
+// a = 2 + 2;
+// b = a + 10;
+// a = a + 2;
+// a += 2; //-= *= /= і тд
+
+//
+// a++; //increment
+// a--; //decrement
+// --a;
+// ++a;
+
+
+let a = '2', b = '5';
+console.log((a - 0) - (b - 0));
+console.log(Number(a) - Number(b));
+
+//інтерполяціЯ !! 1! 11!
+
+let name = 'Ivan', age = 18;
+console.log('Вітаю, ' + name + '! Тобі - ' + age + 'років');
+console.log(`Привіт, ${name}! Тобі - ${age} років`);
+
+
