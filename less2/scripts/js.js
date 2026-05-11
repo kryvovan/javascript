2307d9bbf4d7bd11017ee35d07f16bfb72115600
kryvovan <kryvobokivan0707@gmail.com>
2026-05-11 10:02:34 +0300

    Add files via upload

---
+++ less2/scripts/js.js
@@ -0,0 +1,116 @@
+// console.log('Happy developing ✨')
+// if (вираз){
+//     [інструкції];
+//     [інструкції];
+// }
+
+// false - це все фолс
+// 0 - це все фолс
+// 0n //big integer - це все фолс
+// "" - це все фолс
+// null - це все фолс
+// undefined - це все фолс
+// NaN - це все фолс
+
+//оператори порівняння:
+// let a = 2, b = 3, c = "3";
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a > b);// < >= <=
+//
+// console.log(b === c);
+
+// let a = 2, b = 3, c;
+//
+// if (a > b){
+//     c = "a > b"
+// } else{
+//     c = "a < b"
+// }
+//
+// console.log(c);
+
+//
+// if (a > b){
+//     c = "a > b"
+// } else{
+//     if (a < b){
+//         c = "a < b"
+//     }else{
+//         c = "a === b"
+//     }
+// }
+// console.log(c)
+//
+// if (a > b){
+//     c = "a > b"
+// } else if (a < b) {
+//     c = "a < b"
+// } else{
+//     c = "a === b"
+// }
+// console.log(c)
+
+// if (a > b)c = "a > b";
+// else if (a < b)c = "a < b";
+// else c = "a === b";
+// console.log(c);
+
+
+
+//switch
+//
+// let course, title;
+// course = prompt("What is the title of your project?");
+//
+// switch (course) {
+//     case "javascript":
+//         title = "JavaScript";
+//         break;
+//
+//     case "figma":
+//     case "fgma":
+//         title = "Figma";
+//         break;
+//
+//     case "git":
+//     case "GitHub":
+//     case "Ryuko":
+//         title = "GitHub + Ryuko";
+//         break;
+//     default:
+//         title = "я хз шо ти хоч од мене бери свою тянку і тікай уже задовбау";
+//         break;
+//
+// }
+//
+// alert(title);
+//
+//
+
+
+//логічні оператори
+
+//HE
+// let a = false;
+// console.log(!a);
+//
+// let b = "skibidi rizzler Ryo";
+// console.log(!!b);
+
+// //I
+// let a = true, b = true, c = false;
+//
+// console.log(a && b);
+// console.log(a && c);
+// console.log(c && c);
+//
+// //ABO
+// console.log(b || c);
+
+
+
+
+
+
+
