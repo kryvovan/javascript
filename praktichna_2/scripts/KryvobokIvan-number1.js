046aca824ec8dd5148035c5786506d0f017e9d84
kryvovan <kryvobokivan0707@gmail.com>
2026-05-18 09:57:49 +0300

    Add files via upload

---
+++ praktichna_2/scripts/KryvobokIvan-number1.js
@@ -0,0 +1,15 @@
+// // console.log('Happy developing ✨')
+//
+// let isAutorized, internetSpeed, isBanned, end;
+//
+// isAutorized = prompt("чи авторизований користувач") === true
+//
+// internetSpeed = prompt("швидкість інтернету")
+//
+// isBanned = prompt("чи заблокований акаунт") === false
+//
+// if (isAutorized === true && internetSpeed >= 20 && isBanned === false) {
+//     console.log("ok")
+// } else {
+//     console.warn("no")
+// }
---
+++ praktichna_2/scripts/KryvobokIvan-number3.js
@@ -0,0 +1,13 @@
+// let score, homeworkDone, finalTestPassed;
+//
+// score = prompt("What is your score?");
+// homeworkDone = prompt("Did you do your homework?") === true;
+// finalTestPassed = prompt("Did you pass your test?") === true;
+//
+//
+//
+// if (score >= 70 && homeworkDone === true && finalTestPassed === true ) {
+//     console.log("Сертифікат успішно отримано");
+// } else {
+//     alert("Умови для отримання сертифіката не виконані")
+// }
